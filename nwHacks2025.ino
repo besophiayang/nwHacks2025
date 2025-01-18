62152a12acf43a5f2425ade2e95f5cb1dfaea8cd
Sophia <besophiayang@gmail.com>
2025-01-18 15:38:20 -0800

    Added some Wifi stuff for ESP32

--- nwHacks2025.ino
+++ nwHacks2025.ino
@@ -1,5 +1,11 @@
 #include <Servo.h>
 #include <math.h>
+#include <Wifi.h>
+
+const char* ssid = "ESP32-Network";
+const char* password = "ESP32-Password";
+
+WifiServer server(80); 
 
 Servo coxa1;
 Servo femur1;
@@ -25,10 +31,23 @@ Servo coxa6;
 Servo femur6;
 Servo tibia6;
 
+const double femur_length = 52.5; // mm
+const double tibia_length = 100; // mm
+
+const double Y_Rest = ???;
+const double Z_Rest = ???;
+const double tibia_extra_angle = ???;
+
 void setup() {
   // put your setup code here, to run once:
+
+  WiFi.softAP(ssid,password);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void MoveTo(double X, double Y, double Z) {
+  double 
 }
