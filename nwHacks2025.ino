803fe1528b08ca3f9e2ff1536b03e029192ce075
Sophia <besophiayang@gmail.com>
2025-01-18 14:58:18 -0800

    Added servo labels for all legs

--- nwHacks2025.ino
+++ nwHacks2025.ino
@@ -1,11 +1,34 @@
+#include <Servo.h>
+#include <math.h>
 
+Servo coxa1;
+Servo femur1;
+Servo tibia1;
+
+Servo coxa2;
+Servo femur2;
+Servo tibia2;
+
+Servo coxa3;
+Servo femur3;
+Servo tibia3;
+
+Servo coxa4;
+Servo femur4;
+Servo tibia4;
+
+Servo coxa5;
+Servo femur5;
+Servo tibia5;
+
+Servo coxa6;
+Servo femur6;
+Servo tibia6;
 
 void setup() {
   // put your setup code here, to run once:
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
